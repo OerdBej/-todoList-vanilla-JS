a4e83524fef1761c3e836208db13b8a2f4aec616
oerdbej <oerdbej@gmail.com>
2022-12-22 22:28:57 +0100

    added the remove tasks from local storage functionality

--- script.js
+++ script.js
@@ -97,7 +97,18 @@ function removeTask(e) {
 }
 
 function removeTaskFromLocalStorage(taskItem) {
-  console.log(taskItem);
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (task, index) {
+    if (task.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 //clear the innerHTML tasks list
